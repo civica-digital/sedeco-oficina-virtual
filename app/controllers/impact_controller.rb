d41a1d037287cd57291634f0e388cbef64eb58aa
mikesaurio <mikesaurio@gmail.com>
2015-09-29 20:42:17 -0500

    parse json basico en controlador

--- app/controllers/impact_controller.rb
+++ app/controllers/impact_controller.rb
@@ -1,6 +1,22 @@
 class ImpactController < ApplicationController
- 
-  def index
-  	
+ before_action :get_json
+ require 'json'
+  
+ def index
+  @impacto["impacto"]["array_preguntas"]["pregunta"].each do |pregunta|
+   puts pregunta["numero"]
+   puts pregunta["texto"]
+    pregunta["array_respuestas"]["respuesta"].each do |respuestas|
+      puts respuestas
+    end
+   puts '****************'
   end
+ end
+
+private
+ def get_json
+  source = 'lib/impacto/impacto.json'
+	file = File.read(source)
+  @impacto = ActiveSupport::JSON.decode(file)#JSON.parse(file)
+ end
 end
