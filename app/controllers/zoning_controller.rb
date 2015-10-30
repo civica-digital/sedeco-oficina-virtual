6614f7939a3b79078e680543874926a3bb31fa3c
mikesaurio <mikesaurio@gmail.com>
2015-10-30 17:15:59 -0600

    se valida que si el domicilio mide menos de 20% el arbol siga su flujo

--- app/controllers/zoning_controller.rb
+++ app/controllers/zoning_controller.rb
@@ -21,7 +21,6 @@ class ZoningController < ApplicationController
 
   unless params[:pagetime].blank?    
     next_value = getNext(params[:pagetime][:next],params[:pagetime][:restriction],params[:pagetime][:next_restrictions])
-
     progreso_de_suelo = get_progreso(params[:pagetime][:totals],next_value)
     respond_to do |format|
       if next_value == "-5"
@@ -46,7 +45,7 @@ private
 
   #regresa el id de la vista que continua
  def getNext(next_val, restriction, next_restriction)
- 	if restriction.to_i == -20 && session[:suelo_usuario].to_i == -20
+ 	if restriction.to_i == -20 && session[:uso_de_suelo]
  		next_restriction
  	else
  		next_val
@@ -56,25 +55,90 @@ private
      
   #guarda los valores clickeados por el momento en la session del usuario
   def save_advance(actual, clicked) 
+    case actual.to_i
+      when 1#pregunta 1
+        case clicked.to_i
+          when 1
+            session[:type_zoning] = -1
+          when 2..4
+            session[:type_zoning] = -2
+          when 5
+            session[:type_zoning] = -5
+          when 6
+            session[:type_zoning] = -4
+        else
+          session[:type_zoning] = -4
+        end
+      when 2#pregunta 2
+        case clicked.to_i
+          when 1
+            session[:type_zoning] = -10
+          when 2
+            session[:type_zoning] = -11
+          when 3
+            session[:type_zoning] = -4
+        else
+          session[:type_zoning] = -4
+        end
+      when 5#pregunta 5
+        case clicked.to_i
+          when 1
+            session[:has_zoning] = -12
+          when 2
+            session[:has_zoning] = -13
+          when 3
+            session[:has_zoning] = -4
+        else
+          session[:has_zoning] = -4
+        end
+      when 7#pregunta 7
+        case clicked.to_i
+          when 1
+            session[:has_zoning] = -21
+          when 2..4
+            session[:has_zoning] = -22
+          when 5
+            session[:has_zoning] = -4
+        else
+          session[:has_zoning] = -4
+        end
+
+    else
+    
+    end
   end
 
     #guarda los valores clickeados por el momento en la session del usuario
   def save_advance_rank(actual, clicked, date)
    case actual.to_i
+      when 3 #pregunta 3
+        case clicked.to_i
+          when 2
+            session[:size_house] = 0
+        else
+          session[:size_house] = date
+        end
       when 4 #pregunta 4
         case clicked.to_i
           when 2
             session[:size_business] = 0
         else
           session[:size_business] = date
+          validate_not_zoning
         end
-        
-      when 8 #pregunta 8
+      when 6 #pregunta 6
         case clicked.to_i
           when 2
             session[:date_zoning] = 0
         else
           session[:date_zoning] = date
+        end
+      when 8 #pregunta 8
+        case clicked.to_i
+          when 2
+            session[:city_zoning] = 0
+        else
+          session[:city_zoning] = date
           @lugar = date
         end
       
@@ -87,6 +151,19 @@ private
         end
     else
     
+    end
+  end
+
+
+  def validate_not_zoning
+    unless session[:size_house].nil? || session[:size_house].to_i == 0
+      business = session[:size_business].to_i
+      house = session[:size_house].to_i
+      if (business*100)/house <=20
+       session[:uso_de_suelo] = true
+      else
+        session[:uso_de_suelo] = false
+      end
     end
   end
 
@@ -110,6 +187,8 @@ private
   @array_line=[]
   @array_id=[]
   @lugar= ""
+
+  
 
   @impacto_usuario =  session[:impacto_usuario]
  end
