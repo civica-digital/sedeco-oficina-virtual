80d842f9de146e6e1c0ddc014cab489f0cba3d0a
mikesaurio <mikesaurio@gmail.com>
2015-11-05 17:54:01 -0600

    se hace refactor de helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,19 @@
 module ApplicationHelper
+  
+  #obtiene la imagen svg
+  def prod_img_full(img)
+    "iconos/#{img}.svg"
+  end
 
-	def prod_img_full(img)
-  	"iconos/#{img}.svg"
-	end
-
- 	def clean_arrays
- 	@respuestas_texto_array.clear
+  #limpia los arrays utilizados para llenar las vistas
+  def clean_arrays
+    @respuestas_texto_array.clear
     @respuestas_imagen_array.clear
     @respuestas_siguiente_array.clear
     @respuestas_rango_array.clear
     @respuestas_id_array.clear
     @respuestas_uso_restriccion_array.clear
     @respuestas_siguiente_restriccion_array.clear
- 	end
+  end
 
 end
