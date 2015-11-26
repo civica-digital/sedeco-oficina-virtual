932961c94a8950285d02c0a4a3a111df65cb287e
mikesaurio <mikesaurio@gmail.com>
2015-11-26 13:22:15 -0600

    se agrega el entorno para las salidas generales

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :impact, controller: 'impact'
   resources :zoning, controller: 'zoning'
   resources :safety, controller: 'safety'
+  resources :diagnostic, controller: 'diagnostics'
 
   localized do
     resources :appointments, only: [:index, :create]
