5deeb60394ea298fc72fc37ee8061fd4e24dc530
mikesaurio <mikesaurio@gmail.com>
2015-12-03 18:35:11 -0600

    se recupera titulo de la seccion de preguntas

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -1,7 +1,13 @@
 module QuestionHelper
 
 	def get_help_impact
-		[{title: 'hola',text:'lalaal'},{title: 'hola2',text:'lalaal2'}]
+		[{title: '¿Que es un establecimiento mercantil?',text:'texto'},
+		{title: '¿Qué es impacto vecinal?',text:'texto'},
+		{title: '¿Qué es impacto zonal?',text:'texto'},
+		{title: '¿Que es el SIAPEM?',text:'texto'},
+		{title: '¿En donde encuentro la fecha de registro del SIAPEM?',text:'texto'},
+		{title: '¿Qué es una licencia de funcionamiento?',text:'texto'},
+		{title: 'Qué es una declaración de apertura?',text:'texto'}]
 	end
 
 	def get_help_zoning
