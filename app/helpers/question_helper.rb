c6382d70ef4c554ce0b7f6ee6924a7625f64e3ec
mikesaurio <mikesaurio@gmail.com>
2015-12-03 18:13:24 -0600

    se crean helpers con las ayudas para el usuario

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -4,4 +4,12 @@ module QuestionHelper
 		[{title: 'hola',text:'lalaal'},{title: 'hola2',text:'lalaal2'}]
 	end
 
+	def get_help_zoning
+		[{title: 'hola',text:'lalaal'},{title: 'hola2',text:'lalaal2'}]
+	end
+
+	def get_help_safety
+		[{title: 'hola',text:'lalaal'},{title: 'hola2',text:'lalaal2'}]
+	end
+
 end
