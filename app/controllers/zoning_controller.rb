517b648ed8835e84bc175422ffeed4556ccc2e5d
mikesaurio <mikesaurio@gmail.com>
2015-12-04 13:37:46 -0600

    la salida abre un nuevo tab solo si es necesario

--- app/controllers/zoning_controller.rb
+++ app/controllers/zoning_controller.rb
@@ -33,13 +33,13 @@ class ZoningController < ApplicationController
       progreso_de_suelo = get_progress(params[:pagetime][:totals],next_value)
       respond_to do |format|
         if session[:uso_de_suelo] && next_value == "0"
-          format.js { render :partial => 'shared/outputs/finish_zoning', :locals => {:type => t('outputs.zoning.type_sin_suelo'), :text =>t('outputs.zoning.comment_sin_suelo'),:next_text=>t('outputs.zoning.next_text_sin_suelo'), :path=> "#{safety_index_path}"} }
+          format.js { render :partial => 'shared/outputs/finish_zoning', :locals => {:type => t('outputs.zoning.type_sin_suelo'), :text =>t('outputs.zoning.comment_sin_suelo'),:next_text=>t('outputs.zoning.next_text_sin_suelo'), :path=> "#{safety_index_path}",new_window: false  }}
         elsif !session[:uso_de_suelo] && next_value == "0"
-          format.js { render :partial => 'shared/outputs/finish_zoning', :locals => {:type => t('outputs.zoning.type_con_suelo'), :text =>t('outputs.zoning.comment_con_suelo'),:next_text=>t('outputs.zoning.next_text_con_suelo'), :path=> "#{safety_index_path}"} }
+          format.js { render :partial => 'shared/outputs/finish_zoning', :locals => {:type => t('outputs.zoning.type_con_suelo'), :text =>t('outputs.zoning.comment_con_suelo'),:next_text=>t('outputs.zoning.next_text_con_suelo'), :path=> "#{safety_index_path}",new_window: false  }}
         elsif next_value == "-5"
-          format.js { render :partial => 'shared/outputs/finish_zoning', :locals => {:type => t('outputs.zoning.type_equipamiento'), :text =>t('outputs.zoning.comment_equipamiento'),:next_text=>t('outputs.zoning.next_text_equipamiento'), :path=> "#{appointments_path}"} }
+          format.js { render :partial => 'shared/outputs/finish_zoning', :locals => {:type => t('outputs.zoning.type_equipamiento'), :text =>t('outputs.zoning.comment_equipamiento'),:next_text=>t('outputs.zoning.next_text_equipamiento'), :path=> "#{appointments_path}",new_window: true  }}
         elsif next_value == "-6"
-          format.js { render :partial => 'shared/outputs/finish_zoning', :locals => {:type => t('outputs.zoning.type_derechos'), :text =>t('outputs.zoning.comment_derechos'),:next_text=>t('outputs.zoning.next_text_derechos'), :path=> "#{appointments_path}"} }
+          format.js { render :partial => 'shared/outputs/finish_zoning', :locals => {:type => t('outputs.zoning.type_derechos'), :text =>t('outputs.zoning.comment_derechos'),:next_text=>t('outputs.zoning.next_text_derechos'), :path=> "#{appointments_path}",new_window: true }}
         else  
           format.js { render :js => "hidden_div(#{next_value},100,#{progreso_de_suelo},0);"}
         end
