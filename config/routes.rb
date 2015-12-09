c7b41ecbf23dd6d375887bbf529abee1d0d23dba
mikesaurio <mikesaurio@gmail.com>
2015-12-09 10:25:31 -0600

    se hacen traducción de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   root 'landing#index'
 
-  resources :impact, only: :index, controller: 'impact'
-  resources :zoning, only: :index, controller: 'zoning'
-  resources :safety, only: :index, controller: 'safety'
-  resources :diagnostic, controller: 'diagnostics'do
-      collection do
-        get 'download_diagnostic', :path => "descargar_diagnostico"
-      end
-  end
-
   localized do
+    resources :impact, only: :index, controller: 'impact'
+    resources :zoning, only: :index, controller: 'zoning'
+    resources :safety, only: :index, controller: 'safety'
+    resources :diagnostic, controller: 'diagnostics'do
+        collection do
+          get 'download_diagnostic', :path => "descargar_diagnostico"
+        end
+    end    
     resources :learning_center, only: :index
     resources :appointments, only: [:index, :create]
     devise_for :admins, controllers: { sessions: 'admins/sessions' }
