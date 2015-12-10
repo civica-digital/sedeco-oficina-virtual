b080fdeb5df62dbcc3672c0989f2a8037ec58966
mikesaurio <mikesaurio@gmail.com>
2015-12-10 11:45:36 -0600

    se agrega test impacto

--- spec/features/user_can_see_tree_impact.rb
+++ spec/features/user_can_see_tree_impact.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User can see tree impact'  do
-  scenario 'and he has impacto vecinal', js: true do
+  scenario 'and he has impacto zonal', js: true do
     
     visit "/impacto"
     expect(page).to have_content "¿Ya cuentas con un establecimiento mercantil?"
@@ -10,10 +10,74 @@ describe 'User can see tree impact'  do
     expect(page).to have_content "¿Deseas abrir o tienes un negocio con alguno de estos giros?"
     page.find("#image2-6").click
 
-    expect(page).to have_content "¿Deseas abrir o tienes un negocio con alguno de estos giros?"
+    expect(page).to have_content "¿Deseas abrir o tienes un negocio con alguno de estos giros?",count: 2
     page.find("#image3-1").click
 
     expect(page).to have_content "Impacto Zonal"
 
   end
+
+  scenario 'and he has impacto vecinal', js: true do
+    
+    visit "/impacto"
+    expect(page).to have_content "¿Ya cuentas con un establecimiento mercantil?"
+    page.find("#image1-1").click
+
+    expect(page).to have_content "¿Deseas abrir o tienes un negocio con alguno de estos giros?"
+    page.find("#image2-1").click
+
+    expect(page).to have_content "¿Deseas abrir o tienes un negocio con alguno de estos giros?",count: 1
+
+    expect(page).to have_content "¿Ya registraste tu negocio en el Sistema Electrónico de Avisos y Permisos de Establecimientos Mercantiles (SIAPEM)?"
+    page.find("#image4-2").click
+
+    expect(page).to have_content "¿Cuentas con algún tipo de permiso o licencia de funcionamiento tipo A u ordinaria?"
+    page.find("#image6-2").click
+    
+    expect(page).to have_content "Impacto Vecinal"
+
+  end
+
+
+  scenario 'and he has impacto bajo, NO SIAPEM ', js: true do
+    
+    visit "/impacto"
+    expect(page).to have_content "¿Ya cuentas con un establecimiento mercantil?"
+    page.find("#image1-1").click
+
+    expect(page).to have_content "¿Deseas abrir o tienes un negocio con alguno de estos giros?"
+    page.find("#image2-6").click
+
+    expect(page).to have_content "¿Deseas abrir o tienes un negocio con alguno de estos giros?",count: 2
+    page.find("#image3-5").click
+
+    expect(page).to have_content "¿Ya registraste tu negocio en el Sistema Electrónico de Avisos y Permisos de Establecimientos Mercantiles (SIAPEM)?"
+    page.find("#image4-2").click
+
+    expect(page).to have_content "¿Cuentas con declaración de apertura?"
+    page.find("#image7-3").click
+    
+    expect(page).to have_content "Bajo Impacto"
+
+  end
+
+  scenario 'and he has impacto bajo, SI SIAPEM ', js: true do
+    
+    visit "/impacto"
+    expect(page).to have_content "¿Ya cuentas con un establecimiento mercantil?"
+    page.find("#image1-1").click
+
+    expect(page).to have_content "¿Deseas abrir o tienes un negocio con alguno de estos giros?"
+    page.find("#image2-6").click
+
+    expect(page).to have_content "¿Deseas abrir o tienes un negocio con alguno de estos giros?",count: 2
+    page.find("#image3-5").click
+
+    expect(page).to have_content "¿Ya registraste tu negocio en el Sistema Electrónico de Avisos y Permisos de Establecimientos Mercantiles (SIAPEM)?"
+    page.find("#image4-1").click
+
+    expect(page).to have_content "¿Cuándo te registraste en el SIAPEM?"
+
+  end
+
 end
