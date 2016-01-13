27775a1c5cba156f295111cca810e75c34c68151
mikesaurio <mikesaurio@gmail.com>
2016-01-13 17:24:04 -0600

    se eliminan dias festivos del calendario

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,8 +24,20 @@ $(document).ready(loadChosen);
 $(document).on('page:load', loadChosen);
 $(document).on('page:load', displayModal);
 
+var array_day_out = ["2016-01-01","2016-02-01","2016-03-21","2016-05-01","2016-09-16","2016-11-21","2016-12-25","2017-01-01"]
+
+$('input').datepicker({
+    beforeShowDay: function(date){
+        var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+        return [ array_day_out.indexOf(string) == -1 ]
+    }
+});
 $(function() {
   $("#calendar").datepicker({
+    beforeShowDay: function(date){
+    var string = jQuery.datepicker.formatDate('yy-mm-dd', date);
+    return [ array_day_out.indexOf(string) == -1 ]
+    },
     onSelect: function(date) {
       $("#appointment_scheduled_date").val(date)
       $("#new_appointment").modal();
