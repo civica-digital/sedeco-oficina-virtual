f1a401f5ca9be7693aa65ef7736f591b0c0ae8c0
mikesaurio <mikesaurio@gmail.com>
2016-02-08 18:41:50 -0600

    se modifican validaciones del telefono al hacer iuna cita

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,5 @@
 class Appointment < ActiveRecord::Base
- 	validates :name, :phonenumber, :email, :scheduled_date, presence: true
+ 	validates :name, :email, :scheduled_date, presence: true
+ 	validates_numericality_of :phonenumber, :on => :create,:message => "El teléfono solo debe tener números"
+ 	validates :phonenumber, presence: true, length: { is: 10,:message => "El teléfono debe tener 10 dígitos"}
 end
