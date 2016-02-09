59103c664d12ebd238ba91dbb88e7cbbc4f2c220
mikesaurio <mikesaurio@gmail.com>
2016-02-08 18:54:52 -0600

    se modifica textos del modelo a traducciones

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,5 @@
 class Appointment < ActiveRecord::Base
  	validates :name, :email, :scheduled_date, presence: true
- 	validates_numericality_of :phonenumber, :on => :create,:message => "El teléfono solo debe tener números"
- 	validates :phonenumber, presence: true, length: { is: 10,:message => "El teléfono debe tener 10 dígitos"}
+ 	validates_numericality_of :phonenumber, :on => :create, :message => I18n.t('appointment.errors.numbers')
+ 	validates :phonenumber, presence: true, length: { is: 10, :message => I18n.t('appointment.errors.length')}
 end
